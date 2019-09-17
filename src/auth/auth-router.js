c5d8b6171ad2580cad6a9e1c72b685f788f4c2c3
Mark DeLuzio <deluziomark@gmail.com>
2019-09-17 19:38:23 -0400

    added logic to auth-router.js to check if username and password are correct and if not return an error

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const AuthService = require('./auth-service')
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
@@ -13,7 +14,29 @@ authRouter
               error: `Missing '${key}' in request body`
             })
 
-             res.send('ok')
+        AuthService.getUserWithUserName(
+            req.app.get('db'),
+            loginUser.user_name
+        )
+            .then(dbUser => {
+                if(!dbUser) {
+                    return res.status(400).json({ 
+                        error: `Incorrect user_name or password` 
+                    })
+                }
+
+                return AuthService.comparePasswords(loginUser.password, dbUser.password)
+                    .then(compareMatch => {
+                        if(!compareMatch) {
+                            return res.status(400).json({ 
+                                error: `Incorrect user_name or password` 
+                            })
+                        }
+
+                        res.send('Ok')
+                    })
+            })
+            .catch(next)
     })
 
 module.exports = authRouter
