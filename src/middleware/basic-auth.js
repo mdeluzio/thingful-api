6c9eee6e8c91900d1b4f16214300568b43527398
Mark DeLuzio <deluziomark@gmail.com>
2019-09-17 19:11:22 -0400

    created auth-service.js and refactored basic-auth.js to use AuthService

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
     const authToken = req.get('Authorization') || '';
@@ -10,17 +11,16 @@ function requireAuth(req, res, next) {
         basicToken = authToken.slice('basic '.length, authToken.length)
     }
 
-    const [tokenUserName, tokenPassword] = Buffer
-        .from(basicToken, 'base64')
-        .toString()
-        .split(':')
+    const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
+    
     if(!tokenUserName || !tokenPassword) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     
-    req.app.get('db')('thingful_users')
-        .where({ user_name: tokenUserName})
-        .first()
+    AuthService.getUserWithUserName(
+        req.app.get('db'),
+        tokenUserName
+      )
         .then(user => {
             if(!user) {
                 return res.status(401).json({ error: 'Unauthorized request' })
